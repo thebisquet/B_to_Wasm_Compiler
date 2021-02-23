d8e86d9f6793078d666066537d1bb06661b98c50
Keinen Bousquet <keinen.bousquet@trojans.dsu.edu>
2021-02-23 15:26:50 -0600

    Include lexer file in the main program drivers toolbox.

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,8 @@
 // Basic_Wasm_Compiler.cpp : This file contains the 'main' function. Program execution begins and ends here.
 #include <iostream>
+#include "Lexer.cpp"
 
 using namespace std;
-
 
 int main()
 {
